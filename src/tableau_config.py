4fc855b8573662345f9be1307d0914318126fbdf
Arko Basu <arko.basu@locusview.com>
2020-12-16 15:22:26 -0600

    Doing some clean up and moving functionality of download to separate function

--- src/tableau_config.py
+++ src/tableau_config.py
@@ -1,6 +1,5 @@
 tableau_user = 'tabadmin'
 tableau_passwd = 'locus123View'
-tableau_server_site = ''
 tableau_server = 'https://rca.locusview.com/'
 tableau_server_workbook = 'EXELON FAILED TASK REPORT'
 tableau_server_workbook_id = '943ed122-6028-4aa4-ab62-8b7cf1854122'
--- src/tableau_failed_tasks_reportsender.py
+++ src/tableau_failed_tasks_reportsender.py
@@ -1,11 +1,21 @@
 import tableauserverclient as TSC
 from tableau_config import tableau_user,\
-    tableau_passwd, tableau_server_site,\
-    tableau_server, tableau_server_workbook,\
-    tableau_server_workbook_id
+    tableau_passwd, tableau_server, tableau_server_workbook_id
 
-tableau_auth = TSC.TableauAuth(tableau_user, tableau_passwd, tableau_server_site)
+tableau_auth = TSC.TableauAuth(tableau_user, tableau_passwd, '')
 server = TSC.Server(tableau_server)
+
+def download_and_send(workbook):
+    if not workbook:
+        print("No workbook found!")
+        return
+    try:
+        server.workbooks.populate_views(workbook)
+        print("\nThe views for {0}".format(workbook.name))
+        print([view.name for view in workbook.views])
+    except Exception as e:
+        print("Failure in downloading workbook!\nError: \n{}".format(str(e)))
+
 
 def tableau_export():
 
@@ -13,10 +23,7 @@ def tableau_export():
         with server.auth.sign_in(tableau_auth):
             wb = server.workbooks.get_by_id(tableau_server_workbook_id)
             print("Workbook found!")
-            
-            server.workbooks.populate_views(wb)
-            print("\nThe views for {0}".format(wb.name))
-            print([view.name for view in wb.views])
+            download_and_send(wb)
     except Exception as e:
         print("Could not get Exelon Failed task report workbook!\nError: \n{}".format(str(e)))
 
